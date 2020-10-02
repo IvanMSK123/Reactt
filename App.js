e9668e474609df71d58135bae5e5e729884e1680
IvanMSK123 <67923248+IvanMSK123@users.noreply.github.com>
2020-10-02 02:09:40 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Iteam from './Item.jsx';
+import './App.css';
+
+
+function App() {
+  
+  
+  return (
+    <div className="App">
+      
+      <div>
+        <Iteam />
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
